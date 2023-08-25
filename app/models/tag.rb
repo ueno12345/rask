31a2dc7d30cd5f7bf53a3ded51e092b40c1827d4
Wistlap <plow9lgx@s.okayama-u.ac.jp>
2023-08-25 14:30:31 +0900

    Update tag sorting with single SQL operation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,21 @@ class Tag < ApplicationRecord
   has_many :document_tags, dependent: :destroy
   has_many :documents, through: :document_tags, dependent: :restrict_with_exception
   validates :name, presence: true, uniqueness: true
+
+  def self.order_by_popularity
+    self.find_by_sql <<~'SQL'
+      SELECT
+        tags.*,
+        count(DISTINCT document_tags.id) + count(DISTINCT task_tags.id) AS popularity
+      FROM
+        tags
+        LEFT JOIN document_tags ON tags.id = document_tags.tag_id
+        LEFT JOIN task_tags ON tags.id = task_tags.tag_id
+      GROUP BY
+        tags.id
+      ORDER BY
+        popularity DESC
+      ;
+    SQL
+  end
 end
