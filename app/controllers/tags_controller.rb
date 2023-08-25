00813a7e342d38a02e7a8d19d8d4413798465122
yoshioka <ky19991008@gmail.com>
2023-08-25 11:43:22 +0900

    Make tasks with tasks and documents impossible to be deleted

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -50,10 +50,17 @@ class TagsController < ApplicationController
 
   # DELETE /tags/1 or /tags/1.json
   def destroy
-    @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url, notice: "タグを削除しました．" }
-      format.json { head :no_content }
+      begin
+        @tag.destroy
+        respond_to do |format|
+          format.html { redirect_to tags_url, notice: "タグを削除しました．" }
+          format.json { head :no_content }
+        end
+      rescue ActiveRecord::DeleteRestrictionError => e
+        respond_to do |format|
+          format.html { redirect_to tags_url, alert: @tag.name + "に紐づいているタスク，ドキュメントがあるため削除できません．"}
+          format.json { render json: @tag.errors, status: :precondition_failed}
+      end
     end
   end
 
