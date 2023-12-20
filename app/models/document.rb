4282a72bc09083480eafb0da53e399a0f160bc66
yoshioka <ky19991008@gmail.com>
2023-12-20 11:59:46 +0900

    Update

--- app/models/document.rb
+++ app/models/document.rb
@@ -14,9 +14,10 @@ class Document < ApplicationRecord
   before_validation :add_unique_action_item_marker
 
   def add_unique_action_item_marker
+    return unless self.description
     desc = ""
     self.description.each_line {|line|
-    matched = line.match(/-->\(([^!]+)\)/)
+      matched = line.match(/-->\(([^!]+)\)/)
       if matched != nil
         @action_item = ActionItem.create(task_url: nil)
         line.gsub!(/-->\(.+\)/, "-->(#{matched[1]} !:#{@action_item.uid})")
