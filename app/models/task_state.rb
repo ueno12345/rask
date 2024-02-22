3043ae7aa57abe4e32574d13d38842ca997c8339
hosokawa <knsn1118>
2024-02-22 14:23:57 +0900

    Add 'should delete task_state have no task' and 'should not delete task_state have task'

--- app/models/task_state.rb
+++ app/models/task_state.rb
@@ -1,2 +1,3 @@
 class TaskState < ApplicationRecord
+  has_many :tasks, dependent: :restrict_with_exception
 end
--- test/models/task_state_test.rb
+++ test/models/task_state_test.rb
@@ -16,10 +16,23 @@ class TaskStateTest < ActiveSupport::TestCase
     assert_not TaskState.new(name: "test").valid?
   end
 
-  test "should delete task_state" do
-    skip 'Exception'
-    TaskState.new(name: "test", priority: 0)
-    assert_not TaskState.last.destroy
+  test "should delete task_state have no task" do
+    TaskState.create(name: "test", priority: 0)
+    assert TaskState.last.destroy
+  end
+
+  test "should not delete task_state have task" do
+    TaskState.create(name: "test", priority: 0)
+    Task.create(creator_id: users(:one).id,
+                assigner_id: users(:two).id,
+                due_at: Time.zone.now,
+                project_id: projects(:one).id,
+                content: 'testtask',
+                task_state_id: TaskState.last.id,
+                description: "This is test task")
+    assert_raises(ActiveRecord::DeleteRestrictionError) do
+      TaskState.last.destroy
+    end
   end
 
 end
