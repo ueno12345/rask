c33f29b299f3da4ba7b40244ef1dad194af5309a
hosokawa-kenshin <hosokawa2023@s.okayama-u.ac.jp>
2024-06-21 17:33:00 +0900

    Fix 'should create with correct data' and 'should update description' and 'Should create document with correct data' and 'Should not create document without content'

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -4,14 +4,28 @@ class DocumentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user_template = {
+      name: "Test User",
+      screen_name: "test-user",
+      provider: "test-provider",
+      password_digest: "password",
+      uid: "test-uid",
+      avatar_url: "test-url",
+      active: true
+    }
+  end
 
   test "Should create document with correct data" do
-    skip ''
-    assert Document.new(content: 'test', description: 'test').save
+    user = @user_template.clone
+    Project.create(name: 'testdata', user_id: users(:one).id)
+    document = Document.create(content: 'test', description: 'test', start_at: Time.now,
+                                end_at: Time.now + 1.hour, location: 'Test Location',
+                                user: User.last, creator: User.last, project: Project.last)
+    assert document.valid?
   end
 
   test "Should not create document without content" do
-    skip ''
     assert_not Document.new.save
   end
 
